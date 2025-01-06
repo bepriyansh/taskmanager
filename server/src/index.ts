986f2c1c12b4ea3bf5c2fa7b3206ca131ad2821a
priyaaanshh <priyansh.sahu.cer21@itbhu.ac.in>
2025-01-06 10:24:19 +0530

    fixed vercel deploy

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,28 +5,25 @@ import authRoutes from "./routes/auth";
 import taskRoutes from "./routes/tasks";
 import sayHello from "./routes/sayHello";
 import { errorHandler } from "./utils/responseUtils";
-import cors from "cors";  // Import the cors package
+import cors from "cors";
 
 dotenv.config();
 
 const app = Express();
 
-// Middleware for CORS (Allow cross-origin requests)
-// Allow requests from localhost:3000 and allow credentials
 app.use(
   cors({
-    origin: ['http://localhost:3000','https://taskmanager_client.vercel.app'],  // Allow only these origins
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
-    allowedHeaders: ['Content-Type', 'Authorization'],  // Allowed headers
-    credentials: true,  // Allow credentials (cookies, authorization headers, etc.)
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
   })
 );
 
-// Middleware for JSON parsing
 app.use(Express.json());
 
-// Connect to MongoDB
 ConnectDB();
+
 
 app.use("/", sayHello)
 app.use("/auth", authRoutes);
@@ -34,7 +31,5 @@ app.use("/task", taskRoutes);
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+
+export default app;
